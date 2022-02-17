0c12879549cf1616b47d865b577b6504ec0de647
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-02-17 22:27:08 +0530

    Added product list

---
+++ src/Data.js
@@ -0,0 +1,39 @@
+import Me2 from '../src/img/Me2.png';
+export const products = [
+    {
+        id: 1,
+        img:  Me2 ,
+        link: "https://the-iconic-rihan.github.io/Easy-bank",
+
+    },
+    {
+        id: 2,
+        img: { Me2 },
+        link: "https://the-iconic-rihan.github.io/Easy-bank",
+
+    },
+    {
+        id: 3,
+        img: { Me2 },
+        link: "https://the-iconic-rihan.github.io/Easy-bank",
+
+    },
+    {
+        id: 4,
+        img: { Me2 },
+        link: "https://the-iconic-rihan.github.io/Easy-bank",
+
+    },
+    {
+        id: 5,
+        img:  Me2,
+        link: "https://the-iconic-rihan.github.io/Easy-bank",
+
+    },
+    {
+        id: 6,
+        img: { Me2 },
+        link: "https://the-iconic-rihan.github.io/Easy-bank",
+
+    },
+]
--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -1,26 +1,25 @@
 import React from 'react'
-import '../productList/ProductList.css';
+import '../productList/productList.css';
+import { products } from "../../Data"
 import Product from '../product/Product';
 const ProductList = () => {
     return (
-        <div className=' '>
+        <div className='pl'>
             <div className="pl-texts">
-                <div className="pl-title">
-                    <h1 className="pl-title">
-                        Create & inspire. It's Me
-                    </h1>
-                    <p className="pl-desc">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iste explicabo repellat debitis sunt incidunt quo dolorem odio obcaecati necessitatibus illo officiis repellendus praesentium doloremque possimus numquam dolores, a eaque consequuntur reiciendis atque inventore perspiciatis!
-                    </p>
-                </div>
-                <div className="pl-list">
-                 <Product/>
-                 <Product/>
-                 <Product/>
-                 <Product/>
-                </div>
+                <h1 className="pl-title">
+                    Create & inspire. It's Me
+                </h1>
+                <p className="pl-desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iste explicabo repellat debitis sunt incidunt quo dolorem odio obcaecati necessitatibus illo officiis repellendus praesentium doloremque possimus numquam dolores, a eaque consequuntur reiciendis atque inventore perspiciatis!
+                </p>
+            </div>
+            <div className="pl-list">
+                {products.map((item) => (
+                    <Product key={item.id} img={item.img} link={item.link} />
+                ))}
             </div>
         </div>
+
 
     );
 };
